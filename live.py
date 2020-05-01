9b87340c861b0258ee31b342729c5b603f2f7092
Bazzan <bazzanapalowow@gmail.com>
2020-05-02 00:05:17 +0100

    Project 2 Updated

--- live.py
+++ live.py
@@ -13,19 +13,17 @@ year = input('Select a year to find statistics (1997 to 2010): ')
 ourlist = []
 keylist = []
 
-# Try key, value pair
+i = thedict['CO2 per capita'].index(year)
+for key, value in thedict.items():
+    ourlist.append(float(value[i]))
+    keylist.append(key)
 
-for val in thedict:
-    i = thedict['CO2 per capita'].index(year)
-    ourlist.append(float(thedict[val][i]))
-    keylist.append(val)
 a = ourlist.index(max(ourlist[1:]))
 b = ourlist.index(min(ourlist[1:]))
-
 
 themax = keylist[a]
 themin = keylist[b]
 avg = sum(ourlist[1:])/len(ourlist[1:])
 
 print(f'In {year}, countries with minimum and maximum CO2 emission levels were: [{themin}] and [{themax}] respectively.')
-print(f'Average CO2 emissions in {year} were {avg: 6f}')
+print(f'Average CO2 emissions in {year} were {avg:6f}')
